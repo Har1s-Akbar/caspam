ecbb2dab31734b3ec48d28490d172b2389eb3425
Har1s-Akbar <stanker801@gmail.com>
2022-08-13 07:18:19 -0700

    adjusting header positioning

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,7 +7,7 @@ function Header({data}) {
   const {image, text,} = data[0]
   return (
     <section className='lg:mt-40 mt-16'>
-      <Row align="middle"  justify='end'>
+      <Row align="middle" className='lg:ml-32'>
         <Col  lg={{span: 10}} xs={{span:24}} style={{color: "mintcream"}}>
           <h1 className='lg:text-3xl text-center text-xl' style={{color: 'mintcream'}}>{text[0]}</h1>
           <h1 className='lg:text-3xl text-center text-base' style={{color: 'mintcream'}}>{text[1]}</h1>
@@ -15,7 +15,7 @@ function Header({data}) {
           <h1 className='lg:text-xl text-center italic text-yellow-50'>{text[3]}</h1>
         </Col>
         <Col  lg={{span: 10}}  style={{color: "mintcream"}}>
-          <img className='lg:w-2/3 mt-5 sm:w-11/12 drop-shadow' src={urlFor(image[0])} alt="image" />
+          <img className='lg:w-2/3 mt-5 sm:ml-2 sm:w-11/12 drop-shadow' src={urlFor(image[0])} alt="image" />
         </Col>
       </Row>
       <Row align="middle" justify='end' className='mt-10'>
